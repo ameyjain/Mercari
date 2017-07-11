9c00bb3788589df876c3ee6877844e93dea87c8c
Amey Home <AmeyJain@Shubhis-MacBook-Pro.local>
2017-07-11 17:21:35 -0400

    Setup retrofit services

---
+++ app/src/main/java/com/mercari/mercaritest/net/MercariApi.java
@@ -0,0 +1,20 @@
+package com.mercari.mercaritest.net;
+
+import com.mercari.mercaritest.data.model.Response;
+
+import io.reactivex.Observable;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by AmeyJain on 7/11/17.
+ */
+
+public interface MercariApi {
+
+    String BASE_URL = "PUT_YOUR_BASE_URL_HERE";
+
+    @GET("{category}")
+    Observable<Response> getItems(@Path("category") final String category);
+
+}
---
+++ app/src/main/java/com/mercari/mercaritest/net/MercariServices.java
@@ -0,0 +1,44 @@
+package com.mercari.mercaritest.net;
+
+import android.content.Context;
+
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.GsonBuilder;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by AmeyJain on 7/11/17.
+ */
+
+public class MercariServices {
+
+    private static volatile MercariApi mercariApi;
+
+    public static MercariApi get(final Context applicationContext) {
+        if (mercariApi == null) {
+            synchronized (MercariServices.class) {
+                if (mercariApi == null) {
+
+                    final OkHttpClient client = new OkHttpClient.Builder()
+                            .addInterceptor(new LocalDataInterceptor(applicationContext))
+                            .build();
+
+                    mercariApi = new Retrofit.Builder()
+                            .client(client)
+                            .baseUrl(MercariApi.BASE_URL)
+                            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                            .addConverterFactory(GsonConverterFactory.create(new GsonBuilder().setFieldNamingPolicy
+                                    (FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES).create()))
+                            .build()
+                            .create(MercariApi.class);
+
+                }
+            }
+        }
+        return mercariApi;
+    }
+}
